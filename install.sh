45b3110ef7279ff0126a846861349eee32a1b779
M3GABOY <65479432+M3GABOY@users.noreply.github.com>
2020-06-11 20:14:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,3 +7,4 @@ yes | pip install youtube-dl
 #Installing the script
 wget -P "./bin/" --no-check-certificate "https://raw.githubusercontent.com/M3GABOY/Termux_url_opener_all_in_one_downloader/master/termux-url-opener" 
 chmod +x "./bin/termux-url-opener"
+termux-setup-storage
