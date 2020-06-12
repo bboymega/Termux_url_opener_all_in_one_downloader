67d0222d45158af29777dd07a0468cdd3f955c48
M3GABOY <65479432+M3GABOY@users.noreply.github.com>
2020-06-12 11:36:45 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 mkdir bin
 #Installing dependencies
 pkg update -y && pkg install wget -y && pkg install python -y && pkg install ffmpeg -y
-yes | pip install youtube-dl
+yes | pip install youtube-dl && yes | pip install spotdl
 
 #Installing the script
 wget -P "./bin/" --no-check-certificate "https://raw.githubusercontent.com/M3GABOY/Termux_url_opener_all_in_one_downloader/master/termux-url-opener" 
