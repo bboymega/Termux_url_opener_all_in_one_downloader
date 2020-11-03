2fe55c67893591065c3458d3d6c7c6381a9368b8
M3GABOY <65479432+M3GABOY@users.noreply.github.com>
2020-11-03 14:40:04 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ rm -f "./bin/termux-url-opener"
 mkdir bin
 #Installing dependencies
 pkg update -y && pkg install wget -y && pkg install python -y && pkg install ffmpeg -y
-yes | pip install youtube-dl && yes | pip install spotdl && yes | pip install youtube-dlc
+yes | pip install youtube-dl && yes | pip install spotdl && yes | pip install youtube-dlc && yes | pip install you-get
 
 #Installing the script
 wget -P "./bin/" --no-check-certificate "https://raw.githubusercontent.com/M3GABOY/Termux_url_opener_all_in_one_downloader/master/termux-url-opener" 
