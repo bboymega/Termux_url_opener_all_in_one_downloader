641d36d4490aa4ec66764f7db4a9481857e423be
Bboy Mega <i@bboy.live>
2021-07-16 00:57:02 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,11 +7,11 @@ mkdir ~/bin
 pkg update -y && pkg install wget -y && pkg install python -y && pkg install ffmpeg -y
 yes | pip install youtube-dl && yes | pip install spotdl && yes | pip install yt-dlp && yes | pip install you-get && yes | pip install bs4
 
-wget --no-check-certificate --output-document="~/../usr/bin/rbtv-dl" "https://raw.githubusercontent.com/M3GABOY/rbtv-dl/main/rbtv-dl.py"
-chmod +x "~/../usr/bin/rbtv-dl"
+wget --no-check-certificate --output-document="../usr/bin/rbtv-dl" "https://raw.githubusercontent.com/M3GABOY/rbtv-dl/main/rbtv-dl.py"
+chmod +x "../usr/bin/rbtv-dl"
 
-wget --no-check-certificate --output-document="~/../usr/bin/bandcamp-dl" "https://raw.githubusercontent.com/M3GABOY/bandcamp-dl/main/bandcamp-dl.py"
-chmod +x "~/../usr/bin/bandcamp-dl"
+wget --no-check-certificate --output-document="../usr/bin/bandcamp-dl" "https://raw.githubusercontent.com/M3GABOY/bandcamp-dl/main/bandcamp-dl.py"
+chmod +x "../usr/bin/bandcamp-dl"
 
 #Installing the script
 wget -P ~/bin/ --no-check-certificate "https://raw.githubusercontent.com/M3GABOY/Termux_url_opener_all_in_one_downloader/master/termux-url-opener" 
