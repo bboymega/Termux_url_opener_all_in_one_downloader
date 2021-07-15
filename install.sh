fb95cc62028140782e08adbce79152836f061926
Bboy Mega <i@bboy.live>
2021-07-16 00:50:51 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,10 +7,10 @@ mkdir ~/bin
 pkg update -y && pkg install wget -y && pkg install python -y && pkg install ffmpeg -y
 yes | pip install youtube-dl && yes | pip install spotdl && yes | pip install yt-dlp && yes | pip install you-get && yes | pip install bs4
 
-wget -P "~/../usr/bin" --no-check-certificate --output-document="rbtv-dl" "https://raw.githubusercontent.com/M3GABOY/rbtv-dl/main/rbtv-dl.py"
+wget --no-check-certificate --output-document="~/../usr/bin/rbtv-dl" "https://raw.githubusercontent.com/M3GABOY/rbtv-dl/main/rbtv-dl.py"
 chmod +x "~/../usr/bin/rbtv-dl"
 
-wget -P "~/../usr/bin" --no-check-certificate --output-document="bandcamp-dl" "https://raw.githubusercontent.com/M3GABOY/bandcamp-dl/main/bandcamp-dl.py"
+wget --no-check-certificate --output-document="~/../usr/bin/bandcamp-dl" "https://raw.githubusercontent.com/M3GABOY/bandcamp-dl/main/bandcamp-dl.py"
 chmod +x "~/../usr/bin/bandcamp-dl"
 
 #Installing the script
